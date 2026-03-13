70f3f14f29dc0f3ebbad6d9a1381cf7a6ba2c870
Raraandinii <raraandinib@gmail.com>
2026-03-13 14:35:59 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+// menampilkan tanggal hari ini
+let tanggal = new Date();
+
+document.getElementById("tanggal").innerHTML =
+"Hari ini: " + tanggal.toLocaleString();
+
+
+// alert button
+function tampil(){
+alert("Ini adalah button informasi film");
+}
+
+
+// alert perkenalan
+function show(){
+alert("Film ini menceritakan kisah cinta lintas waktu antara Sore dan Jonathan");
+}
